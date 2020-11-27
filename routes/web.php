0b8fc768c19a39dcb013d42952c0a172eab133cc
Kevin Martinez <kamgmc@gmail.com>
2020-11-27 13:02:31 -0400

    [Feat] Add Home page

--- routes/web.php
+++ routes/web.php
@@ -15,22 +15,33 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+//  Home Page
 Route::get('/', function () {
-    return view('welcome');
-});
+    return Inertia\Inertia::render('Home', [
+        'posts' => Post::with('status')
+            ->whereHas('status', function ($query) {
+                $query->where('status.id', '=', 1);
+            })
+            ->orderByDesc('posts.id')
+            ->get()
+    ]);
+})->name('home');
 
+//  Admin page
 Route::middleware(['auth:sanctum', 'verified'])->get('/admin', function () {
     return Inertia\Inertia::render('Admin', [
         'posts' => Post::with('status')->orderByDesc('id')->get()
     ]);
 })->name('admin');
 
+//  Create new post page
 Route::middleware(['auth:sanctum', 'verified'])->get('/admin/create', function () {
     return Inertia\Inertia::render('Posts/CreatePostForm', [
         'statusList' => Status::all()
     ]);
 })->name('posts.create');
 
+//  Update post page
 Route::middleware(['auth:sanctum', 'verified'])->get('/admin/edit', function () {
     return Inertia\Inertia::render('Posts/EditPostForm', [
         'statusList' => Status::all(),
