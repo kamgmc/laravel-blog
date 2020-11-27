882b36bc0d42a020a51414f1a0a5f6e39f2d3f8b
Kevin Martinez <kamgmc@gmail.com>
2020-11-27 13:01:48 -0400

    [Style] Fix new post button compatibility

--- resources/js/Pages/Admin.vue
+++ resources/js/Pages/Admin.vue
@@ -5,11 +5,12 @@
         Posts
       </h2>
       <inertia-link
-        :href="route('posts.create')"
-        as="button"
-        type="button"
-        class="inline-flex items-center ml-4 px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:shadow-outline-gray transition ease-in-out duration-150">
-        New post
+        :href="route('posts.create')">
+        <button
+          type="button"
+          class="inline-flex items-center ml-4 px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:shadow-outline-gray transition ease-in-out duration-150">
+          New post
+        </button>
       </inertia-link>
     </template>
     <div class="py-12">
