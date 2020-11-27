9c50108eaa9f4fa7f76d4700d93e86433f619a54
Kevin Martinez <kamgmc@gmail.com>
2020-11-27 10:11:02 -0400

    [Feat] Add message for empty posts list

--- resources/js/Pages/Admin.vue
+++ resources/js/Pages/Admin.vue
@@ -14,8 +14,8 @@
     </template>
     <div class="py-12">
       <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-        <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-          <div v-if="posts.length > 0" class="flex flex-col">
+        <div v-if="posts.length > 0" class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+          <div class="flex flex-col">
             <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
               <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
                 <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
@@ -109,6 +109,14 @@
                 </div>
               </div>
             </div>
+          </div>
+        </div>
+        <div v-else class="md:col-span-1 shadow-none">
+          <div class="px-4 sm:px-0 bg">
+            <h3 class="text-lg font-medium leading-6 text-gray-900">You haven't created any posts yet.</h3>
+            <p class="mt-1 text-sm text-gray-600">
+              Start by creating a <inertia-link class="underline" :href="route('posts.create')">new post</inertia-link>
+            </p>
           </div>
         </div>
       </div>
